691a29fd7d54370fc603cfedc230740a356356f4
DAVI DE ARAUJO MOURA DA SILVA <davi.silva2007@cs.udf.edu.br>
2026-09-22 09:49:55 -0300

    subtração foi adicionada.

---
+++ features.py
@@ -0,0 +1,9 @@
+def subtrair (a , b):
+    return a - b
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = subtrair(num1, num2)
+
+print(f"o resultado da subtração é: {resultado} ")
